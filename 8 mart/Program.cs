795ab3e87caa794e8ab5e0fd75a58a4e3bd8eeb1
Radoinov <radoinov08@gmail.com>
2024-03-08 13:23:16 +0200

    mart

---
+++ 8 mart/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _8_mart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vyvedi celi chisla: ");
+           
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int swapVar = arr[i];
+                int index = i;
+                while (index > 0 && arr[index - 1] >= swapVar)
+                {
+                    arr[index] = arr[index - 1];
+                    index--;
+                }
+                arr[index] = swapVar;
+                if (index <= 0)
+                {
+                    Console.WriteLine();
+
+                }
+                
+                
+
+            }
+
+            Console.WriteLine(string.Join(", ", arr));
+            
+
+
+
+
+        }
+    }
+}
